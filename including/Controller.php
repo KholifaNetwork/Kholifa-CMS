c1ed09326df637f865bf95d8c5c0fcd42fcda7ed
Kholifa Network <38209206+kholifaCMS@users.noreply.github.com>
2018-04-09 17:29:45 +0600

    Create Controller.php

---
+++ including/Controller.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package Kholifa CMS
+ *
+ *
+ */
+
+namespace including;
+
+
+/**
+ * General MVC Controller.
+ *
+ * Every plugin controller (public, site or administration controller) must extend this class.
+ * This class has been declared option
+ */
+class Controller
+{
+
+}
