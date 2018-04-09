91a803eac1f7b10c0dd8210d0bb4ce45d752030e
Kholifa Network <38209206+kholifaCMS@users.noreply.github.com>
2018-04-09 16:45:20 +0600

    Create PageStorage.php

---
+++ including/PageStorage.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace including;
+
+
+class PageStorage extends \including\Internal\ValueStorage
+{
+    protected $tableName = 'page_storage';
+    protected $namespaceColumn = 'pageId';
+}
