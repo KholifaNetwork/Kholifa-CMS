a5b70d219501b527c155d024a23ae1b33aaa32bd
kholifaCMS <38209206+kholifaCMS@users.noreply.github.com>
2018-04-09 16:25:27 +0600

    Create ThemeStorage.php

---
+++ including/ThemeStorage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace including;
+
+
+class ThemeStorage extends \including\Internal\ValueStorage
+{
+    protected $tableName = 'theme_storage';
+    protected $namespaceColumn = 'theme';
+}
